7f80b4e458f29ef08f07fccabcd90a2bc7fb65b8
dprieri-ms <dprieri@makingsense.com>
2021-08-30 11:05:48 -0300

    Add GA4 Library

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GA4React from 'ga-4-react';
 import { App } from './modules/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+const ga4react = new GA4React('G-YS98KKBGHF');
+
+(async () => {
+    await ga4react.initialize();
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root'),
+    );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
